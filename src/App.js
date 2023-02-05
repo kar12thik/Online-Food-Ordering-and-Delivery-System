5d09a79275d663d005a5af05351c894f7709c886
MAREENA <m22franc@uwaterloo.ca>
2023-02-05 10:12:06 -0500

    Added routes component to App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-// import logo from "./logo.svg";
+import React  from 'react';
 import "./App.css";
+import AppRoutes from './routes';
 
 function App() {
-  console.log(process.env);
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <h1 className="text-3xl font-bold underline">
-        OFODS - React Dev Environment Setup
-      </h1>
-      <h2>{process.env.REACT_APP_NAME}</h2>
-      <h3>Env : {process.env.REACT_APP_ENV}</h3>
+    <div>
+      <AppRoutes />
     </div>
   );
 }
