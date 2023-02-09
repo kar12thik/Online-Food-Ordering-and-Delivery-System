e64fbd207c8d1ed35297c9f6f581ee81e9d3e5fd
Jayesh Patel <40144196+itsmejay80@users.noreply.github.com>
2023-02-09 06:06:23 +0000

    create process info cards

--- src/components/ProcessInfo.js
+++ src/components/ProcessInfo.js
@@ -1,11 +1,29 @@
 import React from "react";
+import ProcessInfoCard from "./ProcessInfoCard";
 
 function ProcessInfo() {
   return (
-    <div className="border flex">
-      <div className="w-1/3">Choose Restaurant</div>
-      <div className="w-1/3">Choose Food</div>
-      <div className="w-1/3">Pick Up</div>
+    <div>
+      <div className="pt-10 pb-10">
+        <h1 className="text-3xl uppercase font-bold underline">How it Works</h1>
+        <p className="mt-2 mb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        </p>
+      </div>
+      <div className="flex sm:px-0 md:px-14 lg:px-24">
+        <div className="mx-auto flex flex-col md:flex-row lg:flex-row sm:space-x-0 md:space-x-4 lg:space-x-4">
+          <div className="my-4 md:my-0 lg:my-0">
+            <ProcessInfoCard />
+          </div>
+          <div className="my-4 md:my-0 lg:my-0">
+            <ProcessInfoCard />
+          </div>
+          <div className="my-4 md:my-0 lg:my-0">
+            <ProcessInfoCard />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/ProcessInfoCard.js
@@ -0,0 +1,19 @@
+function ProcessInfoCard() {
+  return (
+    <div class=" h-full rounded overflow-hidden shadow-lg">
+      <img
+        className="mx-auto"
+        src="https://img.icons8.com/material-outlined/96/null/restaurant-on-site.png"
+        alt="Restaurants"
+      />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Choose a Restaurants</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ProcessInfoCard;
