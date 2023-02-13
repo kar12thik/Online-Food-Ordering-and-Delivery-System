6955ec85c5e3c3cbad47c198826627035e5d39b7
kj8patel <124079185+kj8patel@users.noreply.github.com>
2023-02-13 04:44:33 +0000

    ECE651-39: Fixed search box which was not  responsive

--- src/components/SearchRest.js
+++ src/components/SearchRest.js
@@ -6,10 +6,10 @@ function SearchRest() {
   <form class="m-auto items-center">
     <label for="simple-search" class="sr-only">Search</label>
     <h1 class="mb-4 items-center text-center text-4xl font-extrabold uppercase leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">Organic Fast Food <br/> Made <br/> Easy and Healthy</h1>
-    <div class="mx-auto flex w-96 content-center justify-center">
+    <div class="mx-auto flex w-auto content-center justify-center">
       <div class=" inset-y-0 flex items-center ">
         <svg aria-hidden="true" class="ml-2 pointer-events-none absolute h-5 w-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-        <input type="text" id="simple-search" class="block w-96 rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500" placeholder="Search Restaurants..." required />
+        <input type="text" id="simple-search" class="block sm:w-auto md:w-80 lg:w-96 rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500" placeholder="Search Restaurants..." required />
       </div>
 
       <button type="submit" class="ml-2 rounded-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -17,11 +17,12 @@ function SearchRest() {
         <span class="sr-only">Search</span>
       </button>
     </div>
-    <div class="mt-2 flex justify-center space-x-8">
-      <div class="hover:underline">Restaurants</div>
-      <div class="hover:underline">Delivery</div>
-      <div class="hover:underline">Reservations</div>
-      <div class="hover:underline">Home Services</div>
+
+    <div class="grid sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4">
+      <div class="">Restaurants</div>
+      <div class="">Delivery</div>
+      <div class="">Reservations</div>
+      <div class="">Home Services</div>
     </div>
   </form>
 </div>
