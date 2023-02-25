10e55d136dd0332619b2b46da0dbb56ba9351114
Karthikeyan Subramanian <k3subram@uwaterloo.ca>
2023-02-25 15:55:41 -0500

    ECE651-42 Created navBar.test.js for testing user story. Fixed Compilation issues due to reference of js attributes

--- src/components/ProcessInfoCard.js
+++ src/components/ProcessInfoCard.js
@@ -1,10 +1,10 @@
 function ProcessInfoCard(props) {
   return (
-    <div class=" h-full overflow-hidden ">
+    <div className=" h-full overflow-hidden ">
       <img className="mx-auto w-20 h-20" src={props.imgUrl} alt="Restaurants" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{props.cardHeading}</div>
-        <p class="text-gray-700 text-base">{props.cardDescription}</p>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{props.cardHeading}</div>
+        <p className="text-gray-700 text-base">{props.cardDescription}</p>
       </div>
     </div>
   );
