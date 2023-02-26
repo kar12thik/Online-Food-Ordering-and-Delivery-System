f93a4501f08f6f6485979350c32c01257a0a0a48
MAREENA <m22franc@uwaterloo.ca>
2023-02-26 02:51:58 -0500

    ECE651-68 - Restaurant details page - Style changes

--- src/screens/RestaurantDetails.js
+++ src/screens/RestaurantDetails.js
@@ -67,17 +67,17 @@ const RestaurantDetails = (props) => {
 
     return (
     <div>
-        <div className="container-fluid">
+        <div className="container-fluid h-72 bg-cover bg-restaurant-cover">
             <div>
-                <div className="container px-0 mx-0">
+                <div className="container p-20 mx-0">
                     <div className="container">
                         <div className="flex">
                             <div className="items-center col-span-2">
-                                <img className="w-60" alt="Food" src={require('../assets/images/rest_image.png')} />
+                                <img className="w-60 scale-75" alt="Food" src={require('../assets/images/rest_image.png')} />
                             </div>
                             <div className="pt-16">
-                                <h1 className="text-2xl font-semibold">Olive Garden</h1>
-                                <p className="">Food, Drinks</p>
+                                <h1 className="text-2xl text-white font-semibold">Olive Garden</h1>
+                                <p className="text-white">Food, Drinks</p>
                             </div>
                         </div>
                     </div>
