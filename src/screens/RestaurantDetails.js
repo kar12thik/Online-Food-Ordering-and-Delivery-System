d944d45ee6803321e6b2b5cdcc4e6b859766c817
MAREENA <m22franc@uwaterloo.ca>
2023-02-28 17:08:14 -0500

    ECE651-68 - Added delete icon, updated class to className, for to htmlFor

--- src/screens/RestaurantDetails.js
+++ src/screens/RestaurantDetails.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { RxCross2 } from "react-icons/rx";
+
 
 const RestaurantDetails = (props) => {
     const [cartItemsList, setcartItemsList] = useState([]);
@@ -31,16 +33,19 @@ const RestaurantDetails = (props) => {
                     <div className="w-2/3 pr-0">
                         <p className="mb-0">Chicken burger</p>
                     </div>
-                    <div className="w-1/3 pl-0 text-right">
-                        <p className="mb-0 font-bold">$12</p>
+                    <div className="w-1/3 pl-0 text-right flex">
+                        <div className="mb-0 font-bold"><span>$14</span></div>
+                        <div className="ml-5 mb-0">< RxCross2 /></div>
                     </div>
                 </div>
                 <div className="flex mb-2 border-b-2">
                     <div className="w-2/3 pr-0">
-                        <p className="mb-0">Pizza</p>
+                        <div className="mb-0"><span>Pizza</span></div>
                     </div>
-                    <div className="w-1/3 pl-0 text-right">
-                        <p className="mb-0 font-bold">$10</p>
+                    <div className="w-1/3 pl-0 text-right flex">
+                        <div className="mb-0 font-bold"><span>$10</span></div>
+                        <div className="ml-5">< RxCross2 /></div>
+                        {/* <p className="mb-0 font-bold"><span className="food-price">RS.{cartItemsList[val].itemPrice}</span><span onClick={() => this.removeCartItem(val)} className="remove-food-item"><FontAwesomeIcon icon="times" /></span></p> */}
                     </div>
                 </div>
             </li>
@@ -89,30 +94,30 @@ const RestaurantDetails = (props) => {
                 <div className="flex mx-auto flex-col md:flex-row lg:flex-row sm:space-x-0 md:space-x-4 lg:space-x-4 pt-10 pl-20 pb-10 ml-10">
                     <div className="w-1/3 justify-center">
                         <div>
-                            <h3 class="mb-4 font-bold text-gray-900 dark:text-white">Categories</h3>
-                            <ul class="w-60 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                                <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                    <div class="flex items-center pl-3">
-                                        <input id="vue-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                                        <label for="vue-checkbox" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Chicken</label>
+                            <h3 className="mb-4 font-bold text-gray-900 dark:text-white">Categories</h3>
+                            <ul className="w-60 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                                    <div className="flex items-center pl-3">
+                                        <input id="vue-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                                        <label htmlFor="vue-checkbox" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Chicken</label>
                                     </div>
                                 </li>
-                                <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                    <div class="flex items-center pl-3">
-                                        <input id="react-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                                        <label for="react-checkbox" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Burgers</label>
+                                <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                                    <div className="flex items-center pl-3">
+                                        <input id="react-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                                        <label htmlFor="react-checkbox" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Burgers</label>
                                     </div>
                                 </li>
-                                <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                    <div class="flex items-center pl-3">
-                                        <input id="angular-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                                        <label for="angular-checkbox" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Pizza</label>
+                                <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                                    <div className="flex items-center pl-3">
+                                        <input id="angular-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                                        <label htmlFor="angular-checkbox" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Pizza</label>
                                     </div>
                                 </li>
-                                <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                    <div class="flex items-center pl-3">
-                                        <input id="laravel-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                                        <label for="laravel-checkbox" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sauces</label>
+                                <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                                    <div className="flex items-center pl-3">
+                                        <input id="laravel-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                                        <label htmlFor="laravel-checkbox" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sauces</label>
                                     </div>
                                 </li>
                             </ul>
@@ -122,7 +127,7 @@ const RestaurantDetails = (props) => {
                         Menu Details Section
                     </div>
                     <div className="w-1/3 justify-center">
-                        <h3 class="mb-4 font-bold text-gray-900 dark:text-white">Your Order</h3>
+                        <h3 className="mb-4 font-bold text-gray-900 dark:text-white">Your Order</h3>
                         <div className="w-60 h-48 p-3 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                         {/* {cartItemsList.length > 0 ? */}
                         {cartItemsCount > 0 ? 
@@ -137,8 +142,8 @@ const RestaurantDetails = (props) => {
                                         <div className="w-2/3 pr-0">
                                             <p className="mb-0">Total</p>
                                         </div>
-                                        <div className="w-1/3 pl-0 text-right">
-                                            <p className="mb-0 font-bold">$22</p>
+                                        <div className="w-1/3 pl-0 text-left">
+                                            <p className="mb-0 font-bold">$24</p>
                                         </div>
                                     </div>
                                 </div>
