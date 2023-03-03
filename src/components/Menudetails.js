07dcb4a192e409f6d733185990f1ab8e4779ae2f
MAREENA <m22franc@uwaterloo.ca>
2023-03-03 14:04:42 -0500

    Changes to address style issues

--- src/components/Menudetails.js
+++ src/components/Menudetails.js
@@ -41,21 +41,21 @@ const MenuDetails = (props) => {
   const [showCartList, setshowCartList] = useState(false);
   const cartItemsCount = 2;
 
-  const addToCart = (item) => {
-    if (item) {
-      cartItemsList.push(item);
-      settotalPrice(totalPrice + Number(item.itemPrice));
-      setcartItemsList(cartItemsList);
-      setshowCartList(true);
-    }
-  };
+  // const addToCart = (item) => {
+  //   if (item) {
+  //     cartItemsList.push(item);
+  //     settotalPrice(totalPrice + Number(item.itemPrice));
+  //     setcartItemsList(cartItemsList);
+  //     setshowCartList(true);
+  //   }
+  // };
 
-  const removeCartItem = (itemIndex) => {
-    const removedItemPrice = Number(cartItemsList[itemIndex].itemPrice);
-    cartItemsList.splice(itemIndex, 1);
-    settotalPrice(totalPrice - removedItemPrice);
-    setcartItemsList(cartItemsList);
-  };
+  // const removeCartItem = (itemIndex) => {
+  //   const removedItemPrice = Number(cartItemsList[itemIndex].itemPrice);
+  //   cartItemsList.splice(itemIndex, 1);
+  //   settotalPrice(totalPrice - removedItemPrice);
+  //   setcartItemsList(cartItemsList);
+  // };
 
   function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
@@ -132,10 +132,10 @@ const MenuDetails = (props) => {
 
   return (
     <div>
-      <div className="container-fluid h-72 bg-cover bg-restaurant-cover">
+      <div className="h-72 bg-cover bg-restaurant-cover">
         <div>
-          <div className="container p-20 mx-0">
-            <div className="container">
+          <div className="p-20 mx-0">
+            <div>
               <div className="flex">
                 <div className="items-center col-span-2">
                   <img
@@ -155,8 +155,8 @@ const MenuDetails = (props) => {
           </div>
         </div>
       </div>
-      <div className="container-fluid bg-slate-200">
-        <div className="container mx-auto">
+      <div className="bg-slate-200">
+        <div className="mr-28">
           <div className="flex mx-auto flex-col md:flex-row lg:flex-row sm:space-x-0 md:space-x-4 lg:space-x-4 pt-10 pl-20 pb-10 ml-10">
             <div className="w-1/3 justify-center">
               <div>
