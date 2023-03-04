f857c236240931e306cea40a70e41de0c353a59a
MAREENA <m22franc@uwaterloo.ca>
2023-03-04 00:39:59 -0500

    ECE651-77 Removed Banner component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Banner from "../components/Banner";
+// import Banner from "../components/Banner";
 import ProcessInfo from "../components/ProcessInfo";
 import FeaturedRest from "../components/FeaturedRest";
 import SearchRest from "../components/SearchRest";
@@ -8,7 +8,7 @@ import OrderNow from "../components/OrderNow";
 function Home() {
   return (
     <div className="App sm:w-screen md:w-screen lg:w-screen flex flex-col">
-      <Banner dataTestId="banner" />
+      {/* <Banner dataTestId="banner" /> */}
       <SearchRest dataTestId="Search_Restaurants" />
       <ProcessInfo dataTestId="Howitworks" />
       <OrderNow dataTestId="OrderNow" />
