e9eebb44afbb70c1c15e4d9268df9462b00d8175
MAREENA <m22franc@uwaterloo.ca>
2023-03-04 19:17:02 -0500

    Order Now section style changes

--- src/components/OrderNow.js
+++ src/components/OrderNow.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function OrderNow({ dataTestId }) {
   return (
     <div className="border" data-testid={dataTestId}>
-      <div className="text-center py-5 home-cont3 text-white h-72 bg-order-now-background">
+      <div className="text-center py-5 home-cont3 text-white h-68 bg-no-repeat bg-order-now-background">
           <p className="mt-20 h1 text-white text-3xl font-bold mt-5 mb-3">JUST ORDER AND WE WILL DELIVER YOU</p>
           <p className="mb-3 text-white">Pellentesque eget justo eget nibh luctus semper at ut tellus.</p>
           <Link to="/restaurants">
