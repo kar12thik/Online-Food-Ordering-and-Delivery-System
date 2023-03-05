2970e7def9e147a9fbf49888bec9448d70c462e1
MAREENA <m22franc@uwaterloo.ca>
2023-03-04 21:31:51 -0500

    ECE651-75 Order Now text changed

--- src/components/OrderNow.js
+++ src/components/OrderNow.js
@@ -6,7 +6,7 @@ function OrderNow({ dataTestId }) {
     <div className="border" data-testid={dataTestId}>
       <div className="text-center py-5 home-cont3 text-white h-68 bg-no-repeat bg-order-now-background">
           <p className="mt-20 h1 text-white text-3xl font-bold mt-5 mb-3">JUST ORDER AND WE WILL DELIVER YOU</p>
-          <p className="mb-3 text-white">Pellentesque eget justo eget nibh luctus semper at ut tellus.</p>
+          <p className="mb-3 text-white">Avoid the lines and have food delivered by us</p>
           <Link to="/restaurants">
             <button type="button" className="btn bg-orange rounded-lg h-12 px-6">
               ORDER NOW
