d55cb48a8d4fec4ee5394619a079c998dc7317b1
MAREENA <m22franc@uwaterloo.ca>
2023-03-04 23:34:10 -0500

    Changes to redirect to top of the Restaurants page

--- src/screens/Restaurants.js
+++ src/screens/Restaurants.js
@@ -1,8 +1,13 @@
-import React from "react";
+import { useEffect, React } from "react";
 import SearchRestOnRestPage from "../components/SearchRestOnRestPage";
 import RestList from "../components/RestList";
 
 function Restaurants() {
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <div>
       <SearchRestOnRestPage dataTestId="Search_Restaurants_On_RestPage"></SearchRestOnRestPage>
