ae6975cbab54d5f64410b2124eed9906324d3f06
kj8patel <124079185+kj8patel@users.noreply.github.com>
2023-03-08 15:33:10 +0000

    ECE651-78: Fixing ui for homepage

--- src/components/FeaturedRestCardsForRestPage.js
+++ src/components/FeaturedRestCardsForRestPage.js
@@ -7,14 +7,14 @@ function FeaturedRestCardsForRestPage(props) {
     <div className="">
       <a
         href="/"
-        className="m-1 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl lg:max-w-xl hover:bg-gray-100 dark:border-gray-700 bg-white-800"
+        className="m-1 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:h-44 lg:h-56 md:max-w-xl lg:max-w-xl hover:bg-gray-100 dark:border-gray-700 bg-white-800"
       >
         <img
           className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
           src={props.restImg}
           alt=""
         />
-        <div className="flex flex-col justify-between leading-normal">
+        <div className="flex ml-4 flex-col justify-between leading-normal">
           <h5 className="mb-1 text-2xl font-bold tracking-tight text-black-900 text-justify">
             {props.restName}
           </h5>
@@ -80,7 +80,7 @@ function FeaturedRestCardsForRestPage(props) {
           </div>
 
           {/* Like Button */}
-          <div className="text-justify mt-6 space-x-14">
+          <div className="text-justify mt-4 space-x-14">
             <button onClick={() => setLike((like) => !like)}>
               <svg
                 width="24"
--- src/components/ProcessInfo.js
+++ src/components/ProcessInfo.js
@@ -9,10 +9,6 @@ function ProcessInfo({ dataTestId }) {
     <div>
       <div className="pt-10 pb-10" data-testid={dataTestId}>
         <h1 className="text-3xl uppercase font-bold underline">How it Works</h1>
-        <p className="mt-2 mb-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        </p>
       </div>
       <div className="flex sm:px-0 md:px-14 lg:px-24">
         <div className="mx-auto flex flex-col md:flex-row lg:flex-row sm:space-x-0 md:space-x-4 lg:space-x-4">
@@ -20,21 +16,21 @@ function ProcessInfo({ dataTestId }) {
             <ProcessInfoCard
               imgUrl={RestaurantImg}
               cardHeading="Choose A Restaurant"
-              cardDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus"
+              cardDescription="Discover new culinary horizons to fill your tummy with our diverse restaurant selection."
             />
           </div>
           <div className="my-4 md:my-0 lg:my-0">
             <ProcessInfoCard
               imgUrl={DishImg}
               cardHeading="Choose A Tasty Dish"
-              cardDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus"
+              cardDescription="Take your taste buds on a journey with our expertly crafted menu from your chosen restaurant."
             />
           </div>
           <div className="my-4 md:my-0 lg:my-0">
             <ProcessInfoCard
               imgUrl={DeliveryImg}
               cardHeading="Pick Up Or Delivery"
-              cardDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus"
+              cardDescription="Satisfy your cravings on your terms with our flexible delivery options."
             />
           </div>
         </div>
