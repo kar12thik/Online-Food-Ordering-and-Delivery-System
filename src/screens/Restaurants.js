dd14d04064f4dd98e5388f39493e50a7fc3368c7
kj8patel <124079185+kj8patel@users.noreply.github.com>
2023-03-13 22:02:48 +0000

    ECE651-71: Pushing the task done till now

--- src/screens/Restaurants.js
+++ src/screens/Restaurants.js
@@ -1,9 +1,11 @@
 import { useEffect, React } from "react";
 import SearchRestOnRestPage from "../components/SearchRestOnRestPage";
 import RestList from "../components/RestList";
+import Data from "../assets/images/data";
+import { useState } from 'react';
 
 function Restaurants() {
-
+  const [item, setItem] = useState(Data);
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
@@ -11,7 +13,7 @@ function Restaurants() {
   return (
     <div>
       <SearchRestOnRestPage dataTestId="Search_Restaurants_On_RestPage"></SearchRestOnRestPage>
-      <RestList dataTestId="Featured_Restaurants"></RestList>
+      <RestList dataTestId="Featured_Restaurants" item={item}></RestList>
     </div>
   );
 }
