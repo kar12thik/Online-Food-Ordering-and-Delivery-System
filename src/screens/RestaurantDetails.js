4d58c8329a7651664ca6c8ef09ec5cc842a80023
Jayesh Patel <40144196+itsmejay80@users.noreply.github.com>
2023-03-15 05:00:39 +0000

    make filter and order details sticky

--- src/screens/RestaurantDetails.js
+++ src/screens/RestaurantDetails.js
@@ -107,8 +107,8 @@ const RestaurantDetails = (props) => {
       <div className="container-fluid bg-slate-200">
         <div className="container mx-auto">
           <div className="flex mx-auto flex-col md:flex-row lg:flex-row sm:space-x-0 md:space-x-4 lg:space-x-4 pt-10 pl-20 pb-10 ml-10">
-            <div className="w-1/4 justify-center">
-              <div>
+            <div className="w-1/4 justify-center ">
+              <div className="sticky top-0">
                 <h3 className="mb-4 font-bold text-gray-900 dark:text-white">
                   Categories
                 </h3>
@@ -183,41 +183,43 @@ const RestaurantDetails = (props) => {
             <div className="w-2/4 justify-center">
               <MenuDetails />
             </div>
-            <div className="w-1/4 justify-center">
-              <h3 className="mb-4 font-bold text-gray-900 dark:text-white">
-                Your Order
-              </h3>
-              <div className="w-60 h-48 p-3 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                {/* {cartItemsList.length > 0 ? */}
-                {cartItemsCount > 0 ? (
-                  <div>
+            <div className="w-1/4 justify-center sticky top-0">
+              <div className="sticky top-0">
+                <h3 className="my-4 font-bold text-gray-900 dark:text-white">
+                  Your Order
+                </h3>
+                <div className="w-60 h-48 p-3 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                  {/* {cartItemsList.length > 0 ? */}
+                  {cartItemsCount > 0 ? (
                     <div>
-                      <ul>{renderCartItemsList()}</ul>
-                    </div>
-                    <div>
-                      <div className="flex mb-2">
-                        <div className="w-2/3 pr-0">
-                          <p className="mb-0">Total</p>
-                        </div>
-                        <div className="w-1/3 pl-0 text-left">
-                          <p className="mb-0 font-bold">$24</p>
+                      <div>
+                        <ul>{renderCartItemsList()}</ul>
+                      </div>
+                      <div>
+                        <div className="flex mb-2">
+                          <div className="w-2/3 pr-0">
+                            <p className="mb-0">Total</p>
+                          </div>
+                          <div className="w-1/3 pl-0 text-left">
+                            <p className="mb-0 font-bold">$24</p>
+                          </div>
                         </div>
                       </div>
                     </div>
+                  ) : (
+                    <p className="text-green-600 p-3">Cart is empty.</p>
+                  )}
+                  <div>
+                    {cartItemsCount !== 0 ? (
+                      <button
+                        type="button"
+                        className="btn rounded-lg ml-10 mt-4 text-white bg-orange p-3"
+                      >
+                        CONFIRM ORDER
+                      </button>
+                    ) : null}
+                    {/* <button type="button" className="btn rounded-lg ml-10 mt-4 text-white bg-orange p-3">CONFIRM ORDER</button> */}
                   </div>
-                ) : (
-                  <p className="text-green-600 p-3">Cart is empty.</p>
-                )}
-                <div>
-                  {cartItemsCount !== 0 ? (
-                    <button
-                      type="button"
-                      className="btn rounded-lg ml-10 mt-4 text-white bg-orange p-3"
-                    >
-                      CONFIRM ORDER
-                    </button>
-                  ) : null}
-                  {/* <button type="button" className="btn rounded-lg ml-10 mt-4 text-white bg-orange p-3">CONFIRM ORDER</button> */}
                 </div>
               </div>
             </div>
