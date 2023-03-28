46d86c533b2cbba4e5da1e7e9a8d1d0a18dc60af
Karthikeyan Subramanian <k3subram@uwaterloo.ca>
2023-03-28 00:11:49 -0400

    Working on cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://quick-food-online.netlify.app/",
+    baseUrl: "http://localhost:3000",
     env: {
       CI: "false",
       REACT_APP_NAME: "Development OFODS",
--- cypress/e2e/login_spec.cy.js
+++ cypress/e2e/login_spec.cy.js
@@ -1,13 +1,47 @@
 /* eslint-disable no-undef */
 describe("payment", () => {
-  it("user can make payment", () => {
+  beforeEach(() => {
     cy.visit("/login");
+
+    cy.get("body").within(() => {
+      cy.get("div").should("contain.text", "Don't have an account yet?");
+    });
     cy.findByRole("textbox", {
       name: /email/i,
-    }).type("hotelone@gmail.com");
-    cy.findByLabelText(/password/i).type("Hotel@123.");
+    }).type("userone@gmail.com");
+    cy.findByLabelText(/password/i).type("Userone@123.");
     cy.findByRole("button", {
       name: /login now/i,
+    }).click();
+    cy.wait(5000);
+  });
+
+  // it("user can log in using restaurant credentials", () => {
+  // cy.visit("/login");
+  //   cy.findByRole("textbox", {
+  //     name: /email/i,
+  //   }).type("hotelone@gmail.com");
+  //   cy.findByLabelText(/password/i).type("Hotel@123.");
+  //   cy.findByRole("button", {
+  //     name: /login now/i,
+  //   }).click();
+  // });
+
+  it("restaurant user can make visit restaurants page", () => {
+    // cy.visit("/login");
+    cy.findByRole("link", {
+      name: /restaurants/i,
+    }).click();
+    cy.get("div.restaurant:first").as("selectedRestaurant");
+    cy.get("@selectedRestaurant").find("button.menu-button").click();
+    cy.findByRole("button", {
+      name: /confirm order/i,
+    }).click();
+    cy.findByRole("button", {
+      name: /ok/i,
+    }).click();
+    cy.findByRole("link", {
+      name: /order requests/i,
     }).click();
   });
 });
