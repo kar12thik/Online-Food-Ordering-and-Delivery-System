d49b5a89c77dce1ddcf7847cd255e21659d1befb
Karthikeyan Subramanian <k3subram@uwaterloo.ca>
2023-03-28 09:45:21 -0400

    Firebase test

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -22,6 +22,11 @@ export const db = firebase.firestore(app);
 
 export default app;
 
+if (process.env.REACT_APP_ENV === "DEVELOPMENT") {
+  firebase.analytics();
+  firebase.crashlytics();
+}
+
 function signUp(userDetails) {
   return new Promise((resolve, reject) => {
     const {
@@ -74,7 +79,7 @@ function signUp(userDetails) {
                 userUid: uid,
                 isRestaurant: isRestaurant,
                 userProfileImageUrl: userProfileImageUrl,
-                typeOfFood: typeOfFood
+                typeOfFood: typeOfFood,
               };
               db.collection("users")
                 .doc(uid)
@@ -227,20 +232,24 @@ function order_request() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         let orderRequest = [];
-        db.collection('users').doc(user.uid).collection("orderRequest").get().then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            const obj = { id: doc.id, ...doc.data() }
-            orderRequest.push(obj);
+        db.collection("users")
+          .doc(user.uid)
+          .collection("orderRequest")
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              const obj = { id: doc.id, ...doc.data() };
+              orderRequest.push(obj);
+            });
+            resolve(orderRequest);
+          })
+          .catch((error) => {
+            reject(error);
           });
-          resolve(orderRequest);
-        })
-        .catch((error) => {
-          reject(error);
-        });
       }
     });
   });
-};
+}
 
 function addItem(itemDetails) {
   const { itemName, itemIngredients, itemPrice, itemCategory, itemImage } =
@@ -298,7 +307,7 @@ function addItem(itemDetails) {
   });
 }
 
-function myFoodList(){
+function myFoodList() {
   return new Promise((resolve, reject) => {
     let user = firebase.auth().currentUser;
     console.log("User =>", user);
@@ -308,20 +317,32 @@ function myFoodList(){
     }
     console.log("uid =>", uid);
     let myFoods = [];
-    db.collection('users').doc(uid).collection('menuItems').get().then((querySnapshot) => {
-      console.log("Inside db.collection");
-      console.log("querySnapshot =>", querySnapshot);
-      querySnapshot.forEach(doc => {
-        const obj = { id: doc.id, ...doc.data() }
-        myFoods.push(obj);
+    db.collection("users")
+      .doc(uid)
+      .collection("menuItems")
+      .get()
+      .then((querySnapshot) => {
+        console.log("Inside db.collection");
+        console.log("querySnapshot =>", querySnapshot);
+        querySnapshot.forEach((doc) => {
+          const obj = { id: doc.id, ...doc.data() };
+          myFoods.push(obj);
+        });
+        resolve(myFoods);
       })
-      resolve(myFoods);
-    }).catch((error) => {
-      reject(error);
-    });
+      .catch((error) => {
+        reject(error);
+      });
   });
 }
 
 // export default firebase;
-export { signUp, logIn, orderNow, restaurant_list, addItem, myFoodList, order_request };
-
+export {
+  signUp,
+  logIn,
+  orderNow,
+  restaurant_list,
+  addItem,
+  myFoodList,
+  order_request,
+};
