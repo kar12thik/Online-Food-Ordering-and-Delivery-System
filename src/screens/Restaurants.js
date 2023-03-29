accc4812e0837c25ae88c901bd78f55fa9d05ce7
Jayesh Patel <40144196+itsmejay80@users.noreply.github.com>
2023-03-29 04:19:01 +0000

    Fix for search

--- src/screens/Restaurants.js
+++ src/screens/Restaurants.js
@@ -1,7 +1,7 @@
 import { useEffect, React } from "react";
 import SearchRestOnRestPage from "../components/SearchRestOnRestPage";
 import RestList from "../components/RestList";
-import { useState } from 'react';
+import { useState } from "react";
 import RestCategories from "../components/RestCategories";
 import { restaurant_list } from "../config/firebase";
 
@@ -11,66 +11,108 @@ function Restaurants() {
   const [item, setItem] = useState(restaurantList);
   const [item2, setItem2] = useState(restaurantList);
   const [categorybar, setCategorybar] = useState(false);
+  const [searchBoxText, setSearchBoxText] = useState("");
+  const [filterCat, setFilterCat] = useState([]);
 
   function filterItem(filterCategory) {
-    if (filterCategory.length > 0) {
-      const newItem = item2.filter((newVal) => {
-        return filterCategory.includes(newVal.category.toLowerCase());
-      });
-      setItem(newItem);
-    }
-    else {
-      setItem(item2);
-    }
-  };
+    setFilterCat(filterCategory);
+    // if (filterCategory.length > 0) {
+    //   const newItem = item2.filter((newVal) => {
+    //     return filterCategory.includes(newVal.category.toLowerCase());
+    //   });
+    //   setItem(newItem);
+    // } else {
+    //   setItem(item2);
+    // }
+  }
 
   function handleSearchBar(searchBoxText) {
-    const searchText = searchBoxText;
-    console.log("searchtext,",searchText);
-    if (restaurantList) {
-        Object.keys(restaurantList).map((val) => { });
-        const result = restaurantList.filter((val) => {
-            return val.userName.toLocaleLowerCase().indexOf(searchText.toLocaleLowerCase()) !== -1;
-        })
-        console.log(result);
-        if (searchText.length > 0) {
-          console.log("inside if");
-          setItem(result);
-          setItem2(result);
-        }
-        else {
-          setItem(restaurantList);
-          setItem2(restaurantList);
-        }
-    }
-  };
+    setSearchBoxText(searchBoxText);
+    // const searchText = searchBoxText;
+    // console.log("searchtext,", searchText);
+    // if (restaurantList) {
+    //   Object.keys(restaurantList).map((val) => {});
+    //   const result = restaurantList.filter((val) => {
+    //     return (
+    //       val.userName
+    //         .toLocaleLowerCase()
+    //         .indexOf(searchText.toLocaleLowerCase()) !== -1
+    //     );
+    //   });
+    //   console.log(result);
+    //   if (searchText.length > 0) {
+    //     console.log("inside if");
+    //     setItem(result);
+    //     setItem2(result);
+    //   } else {
+    //     setItem(restaurantList);
+    //     setItem2(restaurantList);
+    //   }
+    // }
+  }
 
   useEffect(() => {
-    restaurant_list().then((result) => {
-      setRestaurantList(result);
-      let menuItems = [...new Set(result.map((Val) => Val.category.toLowerCase() ))];
-      setMenuItems(menuItems);
-    }).catch((error) => {
-      console.error('Error:', error);
-    });
+    restaurant_list()
+      .then((result) => {
+        setRestaurantList(result);
+        let menuItems = [
+          ...new Set(result.map((Val) => Val.category.toLowerCase())),
+        ];
+        setMenuItems(menuItems);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }, []);
 
   useEffect(() => {
     setItem(restaurantList);
   }, [restaurantList]);
-  
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    var tempItem = [];
+
+    if (filterCat.length > 0) {
+      tempItem = restaurantList.filter((newVal) => {
+        return filterCat.includes(newVal.category.toLowerCase());
+      });
+    } else {
+      tempItem = restaurantList;
+    }
+
+    Object.keys(tempItem).map((val) => {});
+    tempItem = tempItem.filter((val) => {
+      return (
+        val.userName
+          .toLocaleLowerCase()
+          .indexOf(searchBoxText.toLocaleLowerCase()) !== -1
+      );
+    });
+
+    setItem(tempItem);
+  }, [searchBoxText, filterCat, restaurantList]);
+
   return (
     <div>
-      <SearchRestOnRestPage dataTestId="Search_Restaurants_On_RestPage" restaurantList={restaurantList} handleSearchBar={handleSearchBar}></SearchRestOnRestPage>
+      <SearchRestOnRestPage
+        dataTestId="Search_Restaurants_On_RestPage"
+        restaurantList={restaurantList}
+        handleSearchBar={handleSearchBar}
+      ></SearchRestOnRestPage>
       <div className="">
         <div className="container-fluid bg-slate-200 w-full flex mx-auto flex-col md:flex-row lg:flex-row sm:space-x-0 md:space-x-4 lg:space-x-4 pt-10 pb-10 px-3">
           <div className="w-1/3 justify-center md:block">
             <div className="sticky top-0">
-              <RestCategories categorybar={categorybar} setCategorybar={setCategorybar} menuItems={menuItems} filterItem={filterItem}></RestCategories>
+              <RestCategories
+                categorybar={categorybar}
+                setCategorybar={setCategorybar}
+                menuItems={menuItems}
+                filterItem={filterItem}
+              ></RestCategories>
             </div>
           </div>
           <div className="w-full md:w-full lg:w-2/3 justify-center">
