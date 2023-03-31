e5158f9023787064f7e8f95093875c862062cbec
Jayesh Patel <40144196+itsmejay80@users.noreply.github.com>
2023-03-31 19:24:56 +0000

    Display restaurant image and restaurant name on cover

--- src/components/RestDetailsCover.js
+++ src/components/RestDetailsCover.js
@@ -1,25 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 function RestDetailsCover(props) {
   return (
     <div className="container-fluid h-72 bg-cover bg-restaurant-cover">
-        <div>
-            <div className="container p-20 mx-0">
-                <div className="container">
-                    <div className="flex">
-                        <div className="items-center col-span-2">
-                            <img className="w-60 scale-75" alt="Food" src={props.userProfileImageUrl} />
-                        </div>
-                        <div className="pt-16">
-                            <h1 className="text-2xl text-white font-semibold">{props.restName}</h1>
-                            <p className="text-white">{props.restDescription}</p>
-                        </div>
-                    </div>
-                </div>
+      <div>
+        <div className="container p-20 mx-0">
+          <div className="container">
+            <div className="flex">
+              <div className="items-center col-span-2">
+                <img
+                  className="w-36 h-36 scale-75"
+                  alt="Food"
+                  src={props.userProfileImageUrl}
+                />
+              </div>
+              <div className="pt-16">
+                <h1 className="text-2xl text-white font-semibold">
+                  {props.restName}
+                </h1>
+                <p className="text-white">{props.restDescription}</p>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default RestDetailsCover
+export default RestDetailsCover;
