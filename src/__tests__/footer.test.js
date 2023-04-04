3aad72c7df2eabfa16a2038f8ac4d5db45ff8d47
Riya <rs2desai@uwaterloo.ca>
2023-04-04 14:42:35 -0500

    Made changes in footer.test.js

--- src/__tests__/footer.test.js
+++ src/__tests__/footer.test.js
@@ -36,127 +36,102 @@ describe("Wrapping Firebase For Footer Test", () => {
     // firestore.enableNetwork();
   });
 
-  test("full app rendering/navigating to Restaurant page", async () => {
-    // const store = {};
-    firestore.disableNetwork();
+  test("should render social media buttons", (done) => {
     render(
       <Provider store={store}>
         <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    );
-    const user = userEvent.setup();
-    expect(screen.getByText(/FAVOURITE/i)).toBeInTheDocument();
-    expect(screen.queryByText("Featured Restaurants")).toBeNull();
-
-    // verify page content for expected route after navigating
-    await act(async () => {
-      await user.click(screen.getByText("RESTAURANTS"));
-    });
-    expect(screen.getByText("Featured Restaurants")).toBeInTheDocument();
-  });
-
-  test("full app rendering/navigating to Login page", async () => {
-    firestore.disableNetwork();
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    );
-
-    const user = userEvent.setup();
-    // verify page content for default route
-    expect(screen.getByText(/Featured Restaurants/i)).toBeInTheDocument();
-
-    // verify page content for expected route after navigating
-    await act(async () => {
-      await user.click(
-        screen.getByRole("link", {
-          name: /login\/register/i,
-        })
-      );
-    });
-    expect(screen.getByText("Login Your Account")).toBeInTheDocument();
-  });
-
-  test("full app rendering/navigating to Register Restaurant page", async () => {
-    firestore.disableNetwork();
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    );
-
-    const user = userEvent.setup();
-
-    // verify page content for default route
-    expect(screen.getByText(/Login Your Account/i)).toBeInTheDocument();
-
-    // verify page content for expected route after navigating
-    await act(async () => {
-      await user.click(screen.getByText("REGISTER RESTAURANT"));
-    });
-    expect(screen.getByText("Restaurant Name")).toBeInTheDocument();
-  });
-
-  test("should render Quick Food Button", (done) => {
-    firestore.disableNetwork();
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
+          {" "}
           <Footer />
         </BrowserRouter>
       </Provider>
     );
-    const FooterElement = screen.getByText("Quick Food");
-    expect(FooterElement).toBeInTheDocument();
+    //const searchtext = screen.getByTestId("search-text");
+    //expect(screen.getByTestId("ReactLogo")).toBeInTheDocument();
+    expect(
+      screen.getByText("Delivering satisfaction every day")
+    ).toBeInTheDocument();
+    expect(screen.getByText("Facebook")).toBeInTheDocument();
+    expect(screen.getByText("Instagram")).toBeInTheDocument();
+    expect(screen.getByText("Twitter")).toBeInTheDocument();
+    expect(screen.getByText("Dribbble")).toBeInTheDocument();
     done();
   });
 
-  test("should render Restaurants Button", (done) => {
-    firestore.disableNetwork();
+  test("should render all the static buttons row 1", (done) => {
     render(
       <Provider store={store}>
         <BrowserRouter>
+          {" "}
           <Footer />
         </BrowserRouter>
       </Provider>
     );
-    const FooterElement = screen.getByText("RESTAURANTS");
-    expect(FooterElement).toBeInTheDocument();
+    expect(screen.getByText("Company")).toBeInTheDocument();
+
+    expect(screen.getByText("Diversity & Inclusion")).toBeInTheDocument();
+    expect(screen.getByText("About us")).toBeInTheDocument();
+    expect(screen.getByText("Careers")).toBeInTheDocument();
+    expect(screen.getByText("Customer Stories")).toBeInTheDocument();
+    expect(screen.getByText("Online Communities")).toBeInTheDocument();
+
     done();
   });
 
-  test("should render LOGIN/REGISTER Button", (done) => {
-    firestore.disableNetwork();
+  test("should render all the static buttons row 2", (done) => {
     render(
       <Provider store={store}>
         <BrowserRouter>
+          {" "}
           <Footer />
         </BrowserRouter>
       </Provider>
     );
-    const FooterElement = screen.getByText("LOGIN/REGISTER");
-    expect(FooterElement).toBeInTheDocument();
+    expect(screen.getByText("Contact")).toBeInTheDocument();
+
+    expect(screen.getByText("Help & Support")).toBeInTheDocument();
+    expect(screen.getByText("Partner with us")).toBeInTheDocument();
+    expect(screen.getByText("Ride with us")).toBeInTheDocument();
     done();
   });
 
-  test("should render Register Restaurant Button", (done) => {
-    firestore.disableNetwork();
+  test("should render all the static buttons row 3", (done) => {
     render(
       <Provider store={store}>
         <BrowserRouter>
+          {" "}
           <Footer />
         </BrowserRouter>
       </Provider>
     );
-    const FooterElement = screen.getByText("REGISTER RESTAURANT");
-    expect(FooterElement).toBeInTheDocument();
+    expect(screen.getByText("Legal")).toBeInTheDocument();
+
+    expect(screen.getByText("Terms & Conditions")).toBeInTheDocument();
+    expect(screen.getByText("Refund & Cancellation")).toBeInTheDocument();
+    expect(screen.getByText("Privacy Policy")).toBeInTheDocument();
+    expect(screen.getByText("Cookie Policy")).toBeInTheDocument();
+    expect(screen.getByText("Offer Terms")).toBeInTheDocument();
+
+    done();
+  });
+
+  test("should render all the static buttons row 4", (done) => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          {" "}
+          <Footer />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(screen.getByText("We Deliver To")).toBeInTheDocument();
+
+    expect(screen.getByText("Waterloo")).toBeInTheDocument();
+    expect(screen.getByText("Kitchener")).toBeInTheDocument();
+    expect(screen.getByText("Cambridge")).toBeInTheDocument();
+    expect(screen.getByText("Guelph")).toBeInTheDocument();
+    expect(screen.getByText("Toronto")).toBeInTheDocument();
+    expect(screen.getByText("London")).toBeInTheDocument();
+
     done();
   });
 });
